714898b0d642369bbad7a5c1a76489a60ab4ba4b
wangjunchang <919863992@qq.com>
2015-12-05 20:21:35 +0800

    submit

---
+++ routes/users.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/reg', function(req, res, next) {
+    res.render('user/reg',{});
+});
+
+router.post('/reg', function(req, res, next) {
+    var user =  req.body;//读取用户提交过来的注册表单
+    new Model('User')(user).save(function(err,user){
+        if(err){
+            res.redirect('/users/reg');
+        }else{
+            res.redirect('/users/login');
+        }
+    });
+
+});
+
+router.get('/login', function(req, res, next) {
+    res.render('user/login',{});
+});
+router.get('/body', function(req, res, next) {
+    console.log(1);
+    res.render('user/body',{});
+});
+
+router.post('/login', function (req, res) {
+    var user = req.body;
+    Model('User').findOne(user,function(err,user){
+        if(err){
+            console.error(err);
+            return res.redirect('/users/login');
+        }
+        res.redirect('/users/body');//注册成功后返回主页
+    });
+});
+/*router.post('/login', function(req, res, next) {
+  res.send('注册');
+});*/
+
+router.get('/logout', function(req, res, next) {
+  res.send('退出');
+});
+
+module.exports = router;
